17ec08b575f02ff1da720615e2845b50bc14054c
rexqu <junjiequ123@gmail.com>
2023-05-11 16:40:06 -0400

    create disk and lighting

--- honkai_frontend/src/features/character_details/components/GalaxyBackground.js
+++ honkai_frontend/src/features/character_details/components/GalaxyBackground.js
@@ -26,6 +26,7 @@ const GalaxyBackground = () => {
     controls.maxPolarAngle = Math.PI / 2;
     controls.enableZoom = true; // enable zooming in and out
     controls.enableDamping = true; // enable damping for touchpad zooming
+    controls.dampingFactor = 0.1;
 
     // create a sphere geometry and material
     const geometry = new THREE.SphereGeometry(500, 60, 40);
@@ -40,10 +41,42 @@ const GalaxyBackground = () => {
     const sphere = new THREE.Mesh(geometry, material);
     scene.add(sphere);
 
-    // position the camera inside the sphere
-    camera.position.set(0, 0, 0.1);
+    // create a disk geometry and material for the bottom portion
+    const diskGeometry = new THREE.CircleGeometry(1000, 32);
+    const diskMaterial = new THREE.MeshPhongMaterial({ color: 0x1a1a33 }); // Darker blue-gray color
 
+    const disk = new THREE.Mesh(diskGeometry, diskMaterial);
+    // const disk2 = new THREE.Mesh(diskGeometry, diskMaterial);
+
+    disk.rotation.x = -Math.PI / 2; // rotate the disk to face upwards
+    disk.position.set(0, -180, 0); // set the position of the disk
+
+    scene.add(disk);
+
+    // Add directional lights in three corners
+    const light1 = new THREE.DirectionalLight(0xffffff, 2.0);
+    light1.position.set(-1, 1, 1);
+    scene.add(light1);
+
+    const light2 = new THREE.DirectionalLight(0xffffff, 2.0);
+    light2.position.set(0, 1, -1);
+    scene.add(light2);
+
+    const light3 = new THREE.DirectionalLight(0xffffff, 2.0);
+    light3.position.set(1, -1, -1);
+    scene.add(light3);
+
+    const sunlight = new THREE.DirectionalLight(0xffffff, 4.0);
+    sunlight.position.set(0, 1, 1);
+    scene.add(sunlight);
+
+    // Configure shadow settings for the disk
+    disk.receiveShadow = true;
+
+    // position the camera inside the sphere
+    camera.position.set(0, 0, 500);
     // set up a rotation animation for the sphere
+
     function animate() {
       requestAnimationFrame(animate);
       controls.update(); // smooths movement for user movement
